d559b2c616c1b95618bfde6fd3fcad77702b85cb
Ronan MOREL <ronan.morel@slickteam.fr>
2024-10-01 14:09:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Slick TS Utils
+
+## Usage
+
+- Install dependency
+
+```bash
+npm i -S @slickteam/ts-utils
+```
+
+- Exemple import
+
+```ts
+import { roundNumber } from "@slickteam/ts-utils";
+
+function test() {
+  const res = roundNumber(516.528, 2);
+  // res = 516.53
+}
+```
+
+## Content
+
+- Functions:
+  - `roundNumber`
+  - `sleep`
+  - `isBoolean`
+  - `isNumber`
+
+## Dependencies version
+
+None
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+function roundNumber(input: number, precision = 2): number {
+  const factor = Math.pow(10, precision);
+  return Math.round((input + Number.EPSILON) * factor) / factor;
+}
+
+function sleep(duration: number): Promise<void> {
+  return new Promise((r) => setTimeout(r, duration));
+}
+
+function isBoolean(value: string): boolean {
+  return value === "false" || value === "true";
+}
+
+function isNumber(
+  value: number | string | boolean | undefined | null
+): boolean {
+  return value != null && value !== "" && !isNaN(Number(value));
+}
+
+export { roundNumber, sleep, isBoolean, isNumber };
